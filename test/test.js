453479d59d790cdd613aba7a547c04627e430ab2
platy11 <mightymaxmax@gmail.com>
2016-03-01 20:06:23 +1100

    Init

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+  describe('#length', function () {
+    it('should return the length', function () {
+      assert.equal(3, [1,2,3].length);
+      assert.equal(5, [1,2,3,4,5].length);
+    });
+  });
+});
